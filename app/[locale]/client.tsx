be93f2c1f2957a3c9c9bdd8e6a02f87dc38ac4a0
Timur Kramár <timurjan.kramar123@gmail.com>
2024-05-28 10:46:10 +0200

    Remove use search params

--- app/[locale]/client.tsx
+++ app/[locale]/client.tsx
@@ -1,14 +1,8 @@
 "use client";
 
 import { useI18n } from "../../locales/client";
-import { useSearchParams } from "next/navigation";
 
 export default function Client() {
   const t = useI18n();
-  const searchParams = useSearchParams();
-  return (
-    <p>
-      From client: {t("hello")} {searchParams.get("hello")}
-    </p>
-  );
+  return <p>From client: {t("hello")}</p>;
 }
